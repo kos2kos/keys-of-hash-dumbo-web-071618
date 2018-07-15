082483afdd4afe5440ce0757141e10bb0a1c01f2
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-15 23:17:28 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,9 @@ class Hash
   def keys_of(arguments)
     # code goes here
     temp = []
+    
       .each do |key, value| 
-      puts arguments
-      puts key
+      
       if arguments == key.to_s
         temp = temp << person
       end
